9163caaf1d6f185c6eb3507922580a1014ecacb7
ryu-yoshikawa-pro-vision <ryu-yoshikawa@pro-vision.jp>
2025-04-13 18:17:50 +0900

    update 2025-04-13 1817

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,5 +73,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [import("tailwindcss-animate"), import("@tailwindcss/typography")],
 };
